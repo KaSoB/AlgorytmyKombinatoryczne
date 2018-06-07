c30ad7e20beb8efc422c27993aa8fdc2c13bbd8b
KamilSobien <kamil.sobien@gmail.com>
2018-06-07 10:21:58 +0200

    Lesson12 Task3 Task4 + UnitTests

---
+++ AlgorytmyKombinatoryczne/Lesson12.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorytmyKombinatoryczne {
+    public class Lesson12 {
+        /// <summary>
+        /// Napisz program generujący kod Prufera dla zadanego drzewa T o n wierzchołkach
+        /// </summary>
+        public static void Task1(int n, int k) {
+
+        }
+
+        /// <summary>
+        /// Napisz program wyznaczający drzewo odpowiadające zadanemu kodowi Prufera L.
+        /// </summary>
+        public static void Task2(int n, int k) {
+
+        }
+
+        /// <summary>
+        /// Napisz program obliczający rangę kodu Prufera L.
+        /// </summary>
+        public static int Task3(List<int> list, int n) {
+            int r = 0;
+            int p = 1;
+            for (int i = n - 3 ; i >= 0 ; i--) {
+                r += p * (list[i] /*- 1*/); // Pytać czemu -1
+                p *= n;
+            }
+            return r;
+        }
+
+        /// <summary>
+        /// Napisz program wyznaczający kod Prufera długości n − 2 o randze r.
+        /// </summary>
+        public static int[] Task4(int r, int n) {
+            var tmp = new int[n - 2];
+
+            for (int i = n - 3 ; i >= 0 ; i--) {
+                tmp[i] = (r % n); /*+ 1;*/ // pytać czemu + 1
+                r = (r - tmp[i] /*+ 1*/) / n;
+            }
+
+            return tmp;
+        }
+    }
+}
---
+++ UnitTest/TestLesson12.cs
@@ -0,0 +1,48 @@
+﻿using AlgorytmyKombinatoryczne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTest {
+    [TestClass]
+    public class TestLesson12 {
+        [TestMethod]
+        public void Test12Task3_0() {
+            // Arrange
+            // Act 
+            var result = Lesson12.Task3(new List<int>() { 1, 2, 4 }, 5);
+            var expected = 39;
+            // Assert 
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Test12Task3_1() {
+            // Arrange
+            // Act 
+            var result = Lesson12.Task3(new List<int>() { 1, 5, 1, 5, 4 }, 7);
+            var expected = 4204;
+            // Assert 
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Test12Task4_0() {
+            // Arrange
+            // Act 
+            var result = Lesson12.Task4(39, 5);
+            var expected = new int[] { 1, 2, 4 };
+            // Assert 
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Test12Task4_1() {
+            // Arrange
+            // Act 
+            var result = Lesson12.Task4(4204, 7);
+            var expected = new int[] { 1, 5, 1, 5, 4 };
+            // Assert 
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
